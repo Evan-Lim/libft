3a84295036b7d0f4e3774b45b8fee0a2c228215c
Evan-Lim <evanlim804@gmail.com>
2026-09-02 13:39:30 +0800

    ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -2,10 +2,19 @@
 
 size_t	ft_strlen(const char *s)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	while (s[i])
 		i++;
 	return (i);
+}
+
+
+#include <stdio.h>
+int	main(void)
+{
+	char	*str = "Hello World!";
+	printf("%zu\n", ft_strlen(str));
+	return (0);
 }

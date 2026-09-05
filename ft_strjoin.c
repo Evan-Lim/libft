56961f84a311396815147d8f4cb2bc0d56a33dc6
Evan-Lim <evanlim804@gmail.com>
2026-09-05 15:02:57 +0800

    ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,56 @@
+#include "libft.h"
+
+/*
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+*/
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dst;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	dst = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		dst[i + j] = s2[j];
+		j++;
+	}
+	dst[i + j] = '\0';
+	return (dst);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	*res;
+
+	res = ft_strjoin("Hello", " World!");
+	if (res)
+	{
+		printf("%s\n", res);
+		free(res);
+	}
+	return (0);
+}
+*/

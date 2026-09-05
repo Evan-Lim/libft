f433032baefbc51f8a9b7ecd0c131cd6f7ef9d49
Evan-Lim <evanlim804@gmail.com>
2026-09-05 21:48:46 +0800

    ft_putendl_fd

---
+++ ft_putendl_fd.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	size_t	i;
+
+	if (!s)
+		return ;
+	while (s[i])
+		write(fd, &s[i++], 1);
+	write(fd, "\n", 1);
+}
+
+/*
+int	main(void)
+{
+	ft_putendl_fd("This is a string followed by a newline.", 1);
+}
+*/

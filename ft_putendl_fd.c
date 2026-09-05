85c953befc11648617e92d6875989e9f210b2447
Evan-Lim <evanlim804@gmail.com>
2026-09-05 22:05:44 +0800

    mandatory parts done

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,6 +6,7 @@ void	ft_putendl_fd(char *s, int fd)
 
 	if (!s)
 		return ;
+	i = 0;
 	while (s[i])
 		write(fd, &s[i++], 1);
 	write(fd, "\n", 1);
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,6 +6,7 @@ void	ft_putstr_fd(char *s, int fd)
 
 	if (!s)
 		return ;
+	i = 0;
 	while (s[i])
 		write(fd, &s[i++], 1);
 }

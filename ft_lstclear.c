af5d8912b94cee3edb8119d57eec9aead1dfb876
Evan-Lim <evanlim804@gmail.com>
2026-09-09 10:07:33 +0800

    ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,9 +1,98 @@
 #include "libft.h"
 
+/*
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	size_t	len;
+	char	*dst;
+
+	i = 0;
+	len = ft_strlen(s1);
+	dst = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dst)
+		return (NULL);
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+}
+
 void	del_content(void *content)
 {
 	free(content);
 }
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
+
+int	ft_lstsize(t_list *lst)
+{
+	int	count;
+
+	count = 0;
+	while (lst)
+	{
+		count++;
+		lst = lst->next;
+	}
+	return (count);
+}
+*/
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
@@ -19,3 +108,21 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	}
 	*lst = NULL;
 }
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	t_list	*head;
+
+	head = NULL;
+	ft_lstadd_back(&head, ft_lstnew(ft_strdup("Node 1")));
+	ft_lstadd_back(&head, ft_lstnew(ft_strdup("Node 2")));
+	ft_lstadd_back(&head, ft_lstnew(ft_strdup("Node 3")));
+	printf("List created, size: %d\n", ft_lstsize(head));
+	ft_lstclear(&head, del_content);
+	printf("List cleared, head pointer: %p\n", (void *)head);
+	printf("Size after clear: %d\n", ft_lstsize(head));
+	return (0);
+}
+*/
